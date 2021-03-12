5efc904b0e88ac36ac341733e625e54cb152b497
ayobamshy <admit_kunle@yahoo.com>
2021-03-12 15:55:09 -0500

    Force airflow version to 1.10.9

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     name='airflow_dbt',
     version=about['__version__'],
     packages=find_packages(exclude=['tests']),
-    install_requires=['apache-airflow == 1.10.4'],
+    install_requires=['apache-airflow == 1.10.9'],
     author='GoCardless',
     author_email='engineering@gocardless.com',
     description='Apache Airflow integration for dbt',
